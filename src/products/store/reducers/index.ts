820b7c1230ff6a862957f6207eac1635e196d18d
krosenk729 <krosenk729@gmail.com>
2018-04-27 11:16:12 -0400

    combing reducers

--- src/products/store/reducers/index.ts
+++ src/products/store/reducers/index.ts
@@ -1,5 +1,12 @@
+import { ActionReducerMap } from '@ngrx/store';
 import * as fromDogs from './dogs.reducer';
 
+// slice of state
 export interface ProductsState{
 	dogs: fromDogs.DogState
 }
+
+// slice of state managed by reducer function
+export const reducers: ActionReducerMap<ProductsState> = {
+	dogs: fromDogs.reducer
+};;
