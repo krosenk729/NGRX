b86a3bda1a1ac1b8e5f0aa4080887d17ae5b80be
krosenk729 <krosenk729@gmail.com>
2018-04-27 10:44:13 -0400

    adding actions

--- src/products/store/actions/dogs.action.ts
+++ src/products/store/actions/dogs.action.ts
@@ -4,10 +4,20 @@ import { Action } from '@ngrx/store';
 // if fail, dispatch fail
 // if success, dispatch success
 export const LOAD_DOGS = '[Products] Load Dogs';
-export const LOAD_DOGS_Fail = '[Products] Load Dogs Fail';
-export const LOAD_DOGS_Success = '[Products] Load Dogs Success';
+export const LOAD_DOGS_FAIL = '[Products] Load Dogs Fail';
+export const LOAD_DOGS_SUCCESS = '[Products] Load Dogs Success';
 
 // action creators
 export class LoadDogs implements Action{
 	readonly type = LOAD_DOGS;
+}
+
+export class LoadDogsFail implements Action{
+	readonly type = LOAD_DOGS_FAIL;
+	constructor(public payload: any){}
+}
+
+export class LoadDogsSuccess implements Action{
+	readonly type = LOAD_DOGS_SUCCESS;
+	constructor(public payload: any){}
 }
