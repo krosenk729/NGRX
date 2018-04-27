18904397aa6f5d89dda26f7d1f03756881351586
krosenk729 <krosenk729@gmail.com>
2018-04-27 10:58:56 -0400

    adding reducers

--- src/products/store/reducers/dogs.reducer.ts
+++ src/products/store/reducers/dogs.reducer.ts
@@ -1,7 +1,13 @@
+import { Dog } from '../../models/dog.model';
 
 
+export interface DogState {
+	data: Dog[],
+	loaded: boolean,
+	loading: boolean
+}
 
-export const initialState = {
+export const initialState: DogState = {
 	data: [],
 	loaded: false,
 	loading: false
