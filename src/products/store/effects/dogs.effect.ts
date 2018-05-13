76c184343cb30095ad4ccf24c919ffb447a735b3
krosenk729 <krosenk729@gmail.com>
2018-05-13 06:59:31 -0400

    adding catcherror

--- src/products/store/effects/dogs.effect.ts
+++ src/products/store/effects/dogs.effect.ts
@@ -2,7 +2,8 @@ import { Injectable } from '@angular/core';
 // actions are observable -- so we can listen for them and respond
 import { Effect, Actions } from '@ngrx/effects';
 // switchMap allows us to return a new action after mapping/reducing
-import { map, switchMap } from 'rxjs/operators';
+import { map, switchMap, catchError } from 'rxjs/operators';
+import { of } from 'rxjs/observable/of';
 
 import * as fromServices from '../../services';
 import * as dogActions from '../actions/dogs.action';
@@ -22,7 +23,9 @@ export class DogsEffects {
 		.pipe(
 			switchMap(()=>{
 				return this.dogService.getDog().pipe(
-					map(dogs => new dogActions.LoadDogsSuccess(dogs))
+					map(dogs => new dogActions.LoadDogsSuccess(dogs)),
+					// use 'of' to export as variable 
+					catchError(error => of(new dogActions.LoadDogsFail(error)))
 					);
 			})
 		);
