b2c384990f43e200fa90c0cae8a2aa292faa9857
krosenk729 <krosenk729@gmail.com>
2018-05-13 06:57:15 -0400

    adding dog service and switchmap

--- src/products/store/effects/dogs.effect.ts
+++ src/products/store/effects/dogs.effect.ts
@@ -1,15 +1,29 @@
 import { Injectable } from '@angular/core';
-import { Effect, Actions } from '@ngrx/effects';
 // actions are observable -- so we can listen for them and respond
+import { Effect, Actions } from '@ngrx/effects';
+// switchMap allows us to return a new action after mapping/reducing
+import { map, switchMap } from 'rxjs/operators';
 
+import * as fromServices from '../../services';
 import * as dogActions from '../actions/dogs.action';
 
 @Injectable()
 export class DogsEffects {
-	constructor(private actions$: Actions){	}
+	constructor(
+		private actions$: Actions, 
+		private dogService: fromServices.DogService
+		){}
 	
 	// observable stream 
+	// @Effect decorator means we can listen to effect of actions
+	// use pipe on observables to manipulate data
+	@Effect()
 	loadDogs$ = this.actions$.ofType(dogActions.LOAD_DOGS)
-		.map()
-		.filter();
+		.pipe(
+			switchMap(()=>{
+				return this.dogService.getDog().pipe(
+					map(dogs => new dogActions.LoadDogsSuccess(dogs))
+					);
+			})
+		);
 }
