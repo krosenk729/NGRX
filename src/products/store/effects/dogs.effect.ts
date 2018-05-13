f2b75fc825608bb01ce3057da7dc0672af5e7bdc
krosenk729 <krosenk729@gmail.com>
2018-05-13 06:49:28 -0400

    adding effects

--- src/products/store/effects/dogs.effect.ts
+++ src/products/store/effects/dogs.effect.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
 import { Effect, Actions } from '@ngrx/effects';
+// actions are observable -- so we can listen for them and respond
+
+import * as dogActions from '../actions/dogs.action';
 
 @Injectable()
 export class DogsEffects {
-	constructor(){
-
-	}
+	constructor(private actions$: Actions){	}
+	
+	// observable stream 
+	loadDogs$ = this.actions$.ofType(dogActions.LOAD_DOGS)
+		.map()
+		.filter();
 }
